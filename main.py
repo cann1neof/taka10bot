5216c20752504dc9c53ed4ff8a6015dde74dbeb7
Alexander Ikonnikov <cann1neof@gmail.com>
2022-12-23 20:19:42 +0200

    kek

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from handlers.handler_register import register
 from handlers.handler_seewho import get_pair
 from handlers.handler_generation import gen_targets
 
-app = ApplicationBuilder().token(token).build()
-app.add_handler(CommandHandler("start", hello))
-app.add_handler(CommandHandler("register", register))
-app.add_handler(CommandHandler("gen_targets", gen_targets))
-app.add_handler(CommandHandler('seeWho', get_pair))
-app.run_polling()
+def application():
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler("start", hello))
+    app.add_handler(CommandHandler("register", register))
+    app.add_handler(CommandHandler("gen_targets", gen_targets))
+    app.add_handler(CommandHandler('seeWho', get_pair))
+    app.run_polling()
